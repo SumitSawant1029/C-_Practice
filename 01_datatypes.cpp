c7b2728cbaa17627c084107108003c734a4eb5a2
SumitSawant1029 <123000840+SumitSawant1029@users.noreply.github.com>
2024-02-12 20:32:06 +0530

    01_lec_Done

--- 01_datatypes.cpp
+++ 01_datatypes.cpp
@@ -2,8 +2,20 @@
 using namespace std;
 
 int main(){
-    int a;//Declaration
-    a=12;//Initialization
-    cout<<"Size of int"<<sizeof(a)<<endl;
+    int a =12;//Declaration
+    // a=12;//Initialization
+    cout<<"Size of int "<<sizeof(a)<<endl;
 
+    float b =1.2;//Declaration
+    // a=12;//Initialization
+    cout<<"Size of float "<<sizeof(b)<<endl;
+
+    char c = 'b';//Declaration
+    // a=12;//Initialization
+    cout<<"Size of char "<<sizeof(c)<<endl;
+
+    bool d = true;//Declaration
+    // a=12;//Initialization
+    cout<<"Size of bool "<<sizeof(d)<<endl;
+     
 }
