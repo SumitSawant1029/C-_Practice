dd348a3212d92c46f70e82524235a8d1c3c60d70
SumitSawant1029 <123000840+SumitSawant1029@users.noreply.github.com>
2024-02-12 20:42:26 +0530

    02_lec_done

---
+++ 02_datatypes.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int amount = 1000;
+    int amount2 ;
+
+    cout<<"Enter Amount 1 :";
+    cin>>amount;
+    cout<<"Enter Amount 2 :";
+    cin>>amount2;
+
+    cout<<amount2+amount;
+    cout<<"\n";
+    // Output
+    std::cout<<"Hello World\n";
+    return 0;
+}
